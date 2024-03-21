499e4b7f742d37c4b63206fea8ad115d8a09bd00
Python-Is-Long <seancg68@gmail.com>
2024-03-21 16:37:36 -0500

    demo

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # HTML Template Engine example with Python + Flask + Jinja2
 
 Install requierements:
-```bash
+```commandline
 pip install -r requirements.txt
 ```
 
 Run the app:
-```bash
+```commandline
 python app.py
 ```
 
-Open your browser and go to:
-```
-http://localhost:5000/
-```
+Open your web browser and go to:
+http://localhost:5000/sales_report
--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import csv
-import os
 from flask import Flask, render_template
-from pathlib import Path
-from typing import List, Dict, Tuple, Union, Optional, Any
+from typing import List, Dict, Any
 
+# Create a Flask app instance
 app = Flask(__name__, template_folder='templates')
 
-# Read sales data from CSV (replace this with your actual data retrieval method)
+
 def read_sales_data(csv_file: str) -> List[Dict[str, Any]]:
     """Read sales data from a CSV file and return as a list of dictionaries.
 
@@ -16,21 +15,40 @@ def read_sales_data(csv_file: str) -> List[Dict[str, Any]]:
     Returns:
         List[Dict[str, Any]]: List of dictionaries containing sales data.
     """
+    # Initialize an empty list to store sales data
     sales_data: List[Dict[str, Any]] = []
+
+    # Open the CSV file in read mode
     with open(csv_file, 'r') as file:
+        # Create a CSV reader object
         reader = csv.DictReader(file)
+
+        # Iterate through each row in the CSV file
         for row in reader:
-            row: Dict[str, Any] = dict(row)
-            sales_data.append(row)
+            # Convert each row to a dictionary and append it to the sales_data list
+            sales_data.append(dict(row))
+
+    # Return the list of sales data
     return sales_data
 
-# Define route to generate and render HTML report
+
 @app.route('/sales_report')
 def generate_sales_report():
-    # Replace 'sales_data.csv' with your actual file path or database query
+    # Call the read_sales_data function to read sales data from the CSV file
+    # Replace 'sales_data.csv' with the actual path to your CSV file
     sales_data = read_sales_data('sales_data.csv')
+
+    # Calculate the total sales from the sales data
     total_sales = sum(float(item['sales']) for item in sales_data)
-    return render_template('sales_report.html', sales_data=sales_data, total_sales=total_sales)
+
+    # Render the 'sales_report.html' template with the sales data and total sales
+    return render_template(
+        template_name_or_list='sales_report.html',
+        sales_data=sales_data,
+        total_sales=total_sales,
+    )
+
 
 if __name__ == "__main__":
+    # Run the Flask app in debug mode on port 5000
     app.run(debug=True, port=5000)
